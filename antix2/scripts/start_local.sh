b01662ece186aca3968be44db724724f914de369
Will Storey <will@summercat.com>
2011-02-20 14:35:44 -0800

    added script to run the simulation easily on one machine

---
+++ antix2/scripts/start_local.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Start a simulation using only the local machine
+#
+# Usage: ./start_local.sh <number of nodes> <number of teams> <robots per team>
+#
+
+if [ $# -ne 3 ]
+then
+	echo "Usage: $0 <number of nodes> <number of teams> <robots per team>"
+	exit -1
+fi
+
+NUM_NODES=$1
+NUM_TEAMS=$2
+ROBOTS_PER_TEAM=$3
+
+# Start nodes first
+i=0
+while [ $i -lt $NUM_NODES ]
+do
+	neighbour_port=`expr 3000 + $i`
+	gui_port=`expr 4000 + $i`
+	# <neighbour port> <gui port> <node IPC id>
+	./node 127.0.0.1 127.0.0.1 $neighbour_port $gui_port $i &
+	i=`expr $i + 1`
+done
+
+# Then clients/teams
+i=0
+while [ $i -lt $NUM_TEAMS ]
+do
+	# Create one client process for each node
+	node=0
+	while [ $node -lt $NUM_NODES ]
+	do
+		# <#robots> <team id> <node IPC id>
+		./client 127.0.0.1 $ROBOTS_PER_TEAM $i $node &
+		node=`expr $node + 1`
+	done
+
+	i=`expr $i + 1`
+done
+
+echo "Started $NUM_TEAMS teams on $NUM_NODES nodes."
