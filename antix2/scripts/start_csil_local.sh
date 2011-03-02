2c05a1078cf764f413d21eebdeb3449b6097061a
Will Storey <will@summercat.com>
2011-03-02 12:25:57 -0800

    Add some logging and error checking to csil run script

--- antix2/scripts/start_csil_local.sh
+++ antix2/scripts/start_csil_local.sh
@@ -3,7 +3,7 @@
 # This is run from start_csil.sh as it connects to each machine
 #
 
-ANTIX_PATH=/home/wjs2/Antix/antix2
+ANTIX_PATH=/home/$USER/Antix/antix2
 
 if [ $# -ne 3 ]
 then
@@ -14,14 +14,25 @@ fi
 MASTER=$1
 NUM_TEAMS=$2
 ROBOTS_PER_TEAM=$3
+HOST=`hostname`
+
+# Before running simulation, remove old sockets. This can fail if node sockets
+# exist from other users...
+rm -rf /tmp/node*
+if [ $? -ne 0 ]
+then
+	echo "Error: couldn't rm /tmp/node*" > /home/$USER/node.$HOST.log
+	exit -1
+fi
 
 # First start the node for this machine
-$ANTIX_PATH/scripts/run_node.tcl $MASTER $NUM_TEAMS &
+$ANTIX_PATH/scripts/run_node.tcl $MASTER $NUM_TEAMS &> /home/$USER/node.$HOST.log &
 
 # Then start the client processes
+rm /home/$USER/clients.$HOST.log
 count=0
 while [ $count -lt $NUM_TEAMS ]
 do
-  $ANTIX_PATH/scripts/run_client.tcl $ROBOTS_PER_TEAM $count &
+  $ANTIX_PATH/scripts/run_client.tcl $ROBOTS_PER_TEAM $count &>> /home/$USER/clients.$HOST.log &
   count=`expr $count + 1`
 done
