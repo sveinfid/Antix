5917236ae2cdc134f172518c15a55c6f0c9f32f5
Will Storey <will@summercat.com>
2011-03-09 14:15:28 -0800

    Add script for counting processes on many machines

--- antix2/scripts/check_ps.tcl
+++ antix2/scripts/check_ps.tcl
@@ -3,8 +3,16 @@
 # Print readable output of processes for each workstation
 #
 
-if {$argc != 1} {
-	puts "Usage: $argv0 <workstations.txt>"
+set ANTIX_PATH /home/${::env(USER)}/Antix/antix2
+
+if {$argc != 2} {
+	puts "Usage: $argv0 <workstations.txt> <show / count>"
+	exit -1
+}
+
+set type [lindex $argv 1]
+if {$type != "count" && $type != "show"} {
+	puts "Usage: $argv0 <workstations.txt> <show / count>"
 	exit -1
 }
 
@@ -17,6 +25,10 @@ set workstations [split $workstations \n]
 
 foreach w $workstations {
 	puts "${w}:"
- catch {exec -ignorestderr ssh -oStrictHostKeyChecking=no -p 24 ${w}.csil.sfu.ca ps x} output
- puts "\t${output}"
+	if {$type == "show"} {
+		catch {exec -ignorestderr ssh -oStrictHostKeyChecking=no -p 24 ${w}.csil.sfu.ca ps x} output
+	} elseif {$type == "count"} {
+		catch {exec -ignorestderr ssh -oStrictHostKeyChecking=no -p 24 ${w}.csil.sfu.ca $ANTIX_PATH/scripts/count_ps.tcl} output
+	}
+	puts "\t${output}"
 }
---
+++ antix2/scripts/count_ps.tcl
@@ -0,0 +1,9 @@
+#!/usr/bin/env tclsh8.5
+#
+# Count processes of simulation running on current machine; output
+#
+
+catch {exec hostname} host
+catch {exec ps x | grep client | wc -l} cli
+catch {exec ps x | grep node | wc -l} node
+puts "${host}: ${cli} clients, ${node} nodes (poss. incl. grep)"
