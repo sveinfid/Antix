6a7766fcc3b87d6a487cdb918948ff2fac9ea9d6
Will Storey <will@summercat.com>
2011-03-12 08:20:53 -0800

    start_local.sh logs to disk in ~/ instead of stdout

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -25,11 +25,12 @@ do
 	neighbour_port=`expr 3000 + $i`
 	gui_port=`expr 4000 + $i`
 	# <neighbour port> <gui port> <node IPC id>
-	./node 127.0.0.1 127.0.0.1 $neighbour_port $gui_port $i $NUM_TEAMS &
+	./node 127.0.0.1 127.0.0.1 $neighbour_port $gui_port $i $NUM_TEAMS &> ~/node.$i.log &
 	i=`expr $i + 1`
 done
 
 # Then clients/teams
+rm -f ~/clients.local.log
 i=0
 while [ $i -lt $NUM_TEAMS ]
 do
@@ -38,7 +39,7 @@ do
 	while [ $node -lt $NUM_NODES ]
 	do
 		# <#robots> <team id> <node IPC id>
-		./client $ROBOTS_PER_TEAM $i $node &
+		./client $ROBOTS_PER_TEAM $i $node &>> ~/clients.local.log &
 		node=`expr $node + 1`
 	done
 
